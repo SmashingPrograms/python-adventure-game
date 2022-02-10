77378bf1259a984608b2f8a39f0ba85d03e0954b
Daniel Juarez <juarezdaniel@gmail.com>
2022-02-10 14:53:34 -0500

    world_three entrance

--- level_three.py
+++ level_three.py
@@ -7,12 +7,12 @@ def level_3(player1):
         'Will you (A)(choose a drink), (B)(continue to see what other things are to be found), or (C)(see if you can take some coin from the tavern?').upper()
 
     if user_input == 'A':
-        print('You grab a drink, and here about an emience treasure directly North of the tavern')
+        print(player1.name, ' grab a drink, and hear about an emience treasure directly North of the tavern')
         player1.health -= 5
     elif user_input == 'B':
-        print('You travel Northwards in search for this eminence treasure')
+        print(player1.name,'travels Northwards in search for this eminence treasure')
     elif user_input == 'C':
-        print('You head into the tavern for some coin!!!!')
+        print(player1.name, 'heads into the tavern for some coin!!!!')
 
     
     while user_input == 'C':
