c4a6482a9602a2383142710f540996a433fdf2f4
ShaunR23 <shaunradford23@gmail.com>
2022-02-14 11:17:38 -0500

    Fixed inputs

--- level_two.py
+++ level_two.py
@@ -22,12 +22,12 @@ def level_2(player1):
 
     print(f'{player1.name} Arrives in a weird colorful world with walking and flying turtles around. \r\n You see a rather big turltle in the distance who appraoches you and introduces himself as Bowser the King Koopa. \r\n He is looking for reliable help to kidnap the Princess, and offers info on a coin stash belonging to Mario. Bowser asks if you are interested.')
 
-    user_input = input(f'Choose (A) To help Bowser, Choose (B) to decline ')
+    user_input = input(f'Choose (A) To help Bowser, Choose (B) to decline ').upper()
 
     if user_input == 'A':
         print("You arrive at Bowser's Castle. While in the castle waiting room you are approached by a koopa. He claims he is tired of working for Bowser and claims he has info on a huge stash in Bowser's chamber.")
 
-        user_input = input('choose(A) to accept the offer. Choose (B) to decline')
+        user_input = input('choose(A) to accept the offer. Choose (B) to decline').upper()
 
         if user_input == 'A':
             print('A trap opens up beneath you and you fall in a pit of lava.\r\n GAME OVER!!')
@@ -37,11 +37,11 @@ def level_2(player1):
             print("You passed the test. Now you and Bowser discuss your plans for the heist and head out to Mario's hideout. \r\n When you arrive at the hideout you approach Mario and Luigi to distract them. They offer you a giant mushroom which refills your health by 3.") 
             change_exp(5)
             change_health(3)
-            user_input = input('Choose (A) to accept the mushroom. Choose (B) to decline.')
+            user_input = input('Choose (A) to accept the mushroom. Choose (B) to decline.').upper()
 
             if user_input == 'A':
                 print("You start to like Mario and Luigi and you decide to tell them Bowser's plan. \r\n Mario goes to stop Bowser,  Luigi offers you 15 coins for helping.")
-                user_input = input("Choose (A) to accept. Choose (B) to decline and attempt to mug Luigi")
+                user_input = input("Choose (A) to accept. Choose (B) to decline and attempt to mug Luigi").upper()
 
                 if user_input == 'A':
                     print("You gain 5 coins but you lose 10exp for not attempting to steal")
@@ -69,12 +69,12 @@ def level_2(player1):
         print("Bowser hits you with a fireball for 3 damage, but luckily he gets hit with a barrel and knocked out. You are approached by Donkey Kong. \r\n Donkey asks for your help to find Diddy Kong who has been missing. It is assumed it is K-Rools doing. \r\n")
         player1.health -= 3
 
-        user_input = 'Choose (A) to help. Choose (B) to decline'
+        user_input = input('Choose (A) to help. Choose (B) to decline').upper()
 
         if user_input == 'A':
             print("You go with Donkey Kong to K-Rools hideout. After you execute your plan you begin to help Diddy escapse. He accidently reveals the location of their own stash of some sort, but he also mentions where he saw a stash in K-Rools hideout")
 
-            user_input = input("To fight off Diddy and rob Donkey Kongs stash, choose (A). To go rob K-Rool choose (B).")
+            user_input = input("To fight off Diddy and rob Donkey Kongs stash, choose (A). To go rob K-Rool choose (B).").upper()
 
             if user_input == 'A':
                     print("You come across a huge stash but not exactly what you expected. You gain 50 bannanas and max health from eating 2 bannanas")
